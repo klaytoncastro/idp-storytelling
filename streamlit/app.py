85756ac91e61998fa21256b6c36b088799638cc7
Klayton Rodrigues de Castro <36307462+klaytoncastro@users.noreply.github.com>
2023-11-21 09:06:40 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,68 +1,59 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Título do aplicativo
-st.title("Aplicativo Streamlit com Colunas e Barra Lateral")
+st.title("Meu Aplicativo Streamlit com Colunas, Gráficos e Markdown")
 
 # Criar uma estrutura de duas colunas
 col1, col2 = st.columns(2)
 
 # No primeiro bloco de coluna
 with col1:
-    st.header("Conteúdo da Coluna 1")
-    st.write("Esta é a primeira coluna")
+    st.header("Gráfico de Pizza")
+    st.write("Esta é a primeira coluna com um gráfico de pizza.")
+    
+    # Dados para o gráfico de pizza
+    labels = ['Maçãs', 'Bananas', 'Laranjas']
+    sizes = [30, 45, 25]
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    st.pyplot(plt)
 
 # No segundo bloco de coluna
 with col2:
-    st.header("Conteúdo da Coluna 2")
-    st.write("Esta é a segunda coluna")
+    st.header("Gráfico de Barras")
+    st.write("Esta é a segunda coluna com um gráfico de barras.")
+    
+    # Dados para o gráfico de barras
+    categories = ['Categoria A', 'Categoria B', 'Categoria C', 'Categoria D']
+    values = [10, 30, 20, 25]
+    plt.bar(categories, values)
+    st.pyplot(plt)
+
+# Adicionar Markdown na seção 3
+st.header("Seção 3")
+markdown_text = """
+Este é um exemplo de conteúdo em Markdown.
+
+- Você pode **enfatizar** palavras ou criar listas como esta.
+- Também é possível incluir links: [Google](https://www.google.com/).
+- Ou até mesmo imagens:
+![Streamlit](https://www.streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png)
+"""
+st.markdown(markdown_text)
 
 # Adicionar widgets e conteúdo à barra lateral
 st.sidebar.header("Barra Lateral")
 
 # Adicionar um widget de caixa de seleção à barra lateral
-option = st.sidebar.selectbox("Selecione uma opção:", ["Opção 1", "Opção 2", "Opção 3"])
+option_sidebar = st.sidebar.selectbox("Selecione uma opção na Barra Lateral", ["Opção A", "Opção B", "Opção C"])
 
 # Adicionar um botão à barra lateral
-if st.sidebar.button("Clique em mim"):
-    st.sidebar.write("Botão clicado!")
+if st.sidebar.button("Clique na Barra Lateral"):
+    st.sidebar.write("Botão clicado na Barra Lateral!")
 
 # Adicionar um slider à barra lateral
-slider_value = st.sidebar.slider("Selecione um valor", 0, 100, 50)
+slider_value = st.sidebar.slider("Selecione um valor na Barra Lateral", 0, 100, 50)
 
 # Adicionar um gráfico à barra lateral
 st.sidebar.line_chart({"data": [1, 2, 3, 4, 5]})
-
-"""
-
-import streamlit as st
-
-# Título do aplicativo
-st.title("Meu Primeiro Aplicativo Streamlit")
-
-# Título da seção
-st.header("Seção 1")
-
-# Texto simples
-st.write("Este é um exemplo simples de um aplicativo Streamlit.")
-
-# Adicionando um gráfico
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Gerando alguns dados aleatórios
-data = np.random.randn(1000)
-
-# Criando um histograma
-fig, ax = plt.subplots()
-ax.hist(data, bins=20)
-
-# Exibindo o gráfico no Streamlit
-st.pyplot(fig)
-
-# Adicionando uma opção de seleção
-option = st.selectbox("Selecione uma opção", ["Opção 1", "Opção 2", "Opção 3"])
-
-# Exibindo a opção selecionada
-st.write("Você selecionou:", option)
-
-"""
