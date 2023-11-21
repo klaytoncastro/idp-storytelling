0e154dce7e0e8ad445b166c60f1c0c6d0e315f43
Klayton Rodrigues de Castro <36307462+klaytoncastro@users.noreply.github.com>
2023-11-21 13:22:33 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Título do aplicativo
-st.title("Meu Aplicativo Streamlit")
+st.title("Meu Primeiro Aplicativo Streamlit")
 
 # Criar uma estrutura de duas colunas
 col1, col2 = st.columns(2)
@@ -11,36 +12,44 @@ col1, col2 = st.columns(2)
 with col1:
     st.header("Gráfico de Pizza")
     st.write("Esta é a primeira coluna com um gráfico de pizza.")
-    
+
     # Dados para o gráfico de pizza
-    data = {'Categorias': ['Maçãs', 'Bananas', 'Laranjas'], 'Valores': [30, 45, 25]}
-    df = pd.DataFrame(data)
-    
-    # Criar um gráfico de pizza com o Plotly
-    fig_pie = px.pie(df, values='Valores', names='Categorias')
-    st.plotly_chart(fig_pie)
+    labels = ['Maçãs', 'Bananas', 'Laranjas']
+    sizes = [30, 45, 25]
+    fig_pie, ax_pie = plt.subplots()
+    ax_pie.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+
+    # Configurar aspect ratio para tornar o gráfico de pizza circular
+    ax_pie.set_aspect('equal')
+
+    st.pyplot(fig_pie)
 
 # No segundo bloco de coluna
 with col2:
     st.header("Gráfico de Barras")
     st.write("Esta é a segunda coluna com um gráfico de barras.")
-    
+
     # Dados para o gráfico de barras
     categories = ['Categoria A', 'Categoria B', 'Categoria C', 'Categoria D']
     values = [10, 30, 20, 25]
-    fig_bar = px.bar(x=categories, y=values)
-    st.plotly_chart(fig_bar)
+    fig_bar, ax_bar = plt.subplots()
+    ax_bar.bar(categories, values)
+    st.pyplot(fig_bar)
 
-# Adicionar Markdown na seção 3
-st.header("Seção 3")
+# Adicionar Markdown
+st.header("Trecho em Markdown")
 markdown_text = """
-Este é um exemplo de conteúdo em Markdown.
+Este é um exemplo de conteúdo em Markdown. Você pode formatar títulos de seções conforme abaixo:
 
-- Você pode **enfatizar** palavras ou criar listas como esta.
-- Também é possível incluir links: [Google](https://www.google.com/).
-- Ou até mesmo imagens:
-![Streamlit](https://www.streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png)
+# Título Grande
+## Título Médio
+### Subtítulo
+
+- Você também pode **enfatizar** palavras, colocá-las em *itálico* ou criar listas como esta.
+- É possível incluir links: [Google](https://www.google.com/).
+- Ou, ainda, imagens: ![Logo](https://images.ecency.com/p/C3TZR1g81UNbFzApDTmvFatkwuDREJFVGiTUu9JN1JupzFWrWjCXzkxpcAdZdG64uv1GM5m4qvUwx3T2geEoHyBfmXu3UFwEcgv3E5vfT8n9be5bvsUik4N?format=match&mode=fit&width=200&height=100)
 """
+
 st.markdown(markdown_text)
 
 # Adicionar widgets e conteúdo à barra lateral
